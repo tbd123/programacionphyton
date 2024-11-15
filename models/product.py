70dfde7b435ec33fb048294d937e67575707ac16
camilo <zuko24@gmail.com>
2024-11-15 18:56:20 -0500

    primer avance

---
+++ models/product.py
@@ -0,0 +1,14 @@
+from config import db
+class Product(db.Model):
+
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price
+        }
